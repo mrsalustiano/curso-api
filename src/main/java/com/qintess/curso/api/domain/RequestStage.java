b48aca621ff0cebf3634d7c9d4884a36bf8ad636
Marcelo  Silva <mrsalustiano@gmail.com>
2020-05-14 06:08:26 -0300

    Update RequestStage.java

--- src/main/java/com/qintess/curso/api/domain/RequestStage.java
+++ src/main/java/com/qintess/curso/api/domain/RequestStage.java
@@ -1,5 +1,24 @@
 package com.qintess.curso.api.domain;
 
-public class RequestStage {
+import java.util.Date;
 
+import com.qintess.curso.api.domain.enums.RequestState;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestStage {
+	
+
+	private Long id;
+	private String description;
+	private Date realizationDate;
+	private RequestState state;
+	private Request request;
+	private User user;
 }

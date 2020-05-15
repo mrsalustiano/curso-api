00b01f8a4f5be2fc3669e2cca339d9f87c01c6b1
Marcelo  Silva <mrsalustiano@gmail.com>
2020-05-15 01:24:15 -0300

    Services

--- src/main/java/com/qintess/curso/api/service/RequestService.java
+++ src/main/java/com/qintess/curso/api/service/RequestService.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.qintess.curso.api.domain.Request;
@@ -13,6 +14,7 @@ import com.qintess.curso.api.repository.RequestRepository;
 @Service
 public class RequestService {
 
+	@Autowired
 	private RequestRepository repository;
 	
 	public Request save(Request request) {
---
+++ src/main/java/com/qintess/curso/api/service/RequestStageService.java
@@ -0,0 +1,50 @@
+package com.qintess.curso.api.service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qintess.curso.api.domain.RequestStage;
+import com.qintess.curso.api.domain.enums.RequestState;
+import com.qintess.curso.api.repository.RequestRepository;
+import com.qintess.curso.api.repository.RequestStageRepository;
+
+@Service
+public class RequestStageService {
+
+	@Autowired
+	private RequestStageRepository repo;
+	
+	@Autowired
+	private RequestRepository repoRequest;
+	
+	
+	public RequestStage save(RequestStage requestStage) {
+		requestStage.setRealizationDate(new Date());
+		
+		RequestStage createStage = repo.save(requestStage);
+		
+		Long requestId = requestStage.getId();
+		RequestState state = requestStage.getState();
+		
+		repoRequest.UpdateStatus(requestId, state);
+		
+		return createStage;
+		
+	}
+
+	public RequestStage getById(Long id) {
+		Optional<RequestStage> result = repo.findById(id);
+		return result.get();
+	}
+	
+	public List<RequestStage> listAllByRequestId(Long id){
+		List<RequestStage> lista = repo.findAllByRequestId(id);
+		return lista;	
+	}
+
+	
+}
